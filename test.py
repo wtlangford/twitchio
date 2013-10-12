58af5a609a758fc6991d9253a927ecb75869f644
acerspeedracer <ace6598@gmail.com>
2013-10-12 17:01:45 -0400

    made twilio send sms

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print "hi"
+from twilio.rest import TwilioRestClient
+import yaml
+
+config = yaml.load(open('config.yaml'))
+account_sid = config['account_sid']
+auth_token = config['auth_token']
+client = TwilioRestClient(account_sid, auth_token)
+message = client.sms.messages.create(body="TEST", to="+17326681916", from_="+17325202749")
+print message.sid
