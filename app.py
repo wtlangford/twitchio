b84e10a26ab6fd9efe1f7aeeae4be6b449bd6a4f
acerspeedracer <ace6598@gmail.com>
2013-10-13 12:49:34 +0000

    Fixed things i think

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ client = TwilioRestClient(account_sid, auth_token)
 listChannels = database.getChannelsToCheck()
 
 for channel in listChannels :
-	r = requests.get("https://api.twitch.tv/kraken/streams/"+channel)
-	#pp(r.json()['stream'])
-	if r.json()['stream'] != None :
-		listNumbers = database.numbersForChannel(channel)
-		for number in listNumbers :
-			message = client.sms.messages.create(body=channel+" is online.", to="+1"+number, from_="17325202749")
-			print message.sid
+  r = requests.get("https://api.twitch.tv/kraken/streams/"+channel)
+  #pp(r.json()['stream'])
+  if r.json()['stream'] == None :
+    database.channelDidStopStreaming(channel)
+  else :
+    database.channelDidStartStreaming(channel)
+  if database.isChannelNewlyStreaming(channel) :
+    listNumbers = database.numbersForChannel(channel)
+    for number in listNumbers :
+      message = client.sms.messages.create(body=channel+" is online.", to="+1"+number, from_="17325202749")
+      print message.sid
