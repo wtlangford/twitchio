14c7252f7b0d80258f3d8f3690d598b86869b1ec
acerspeedracer <ace6598@gmail.com>
2013-10-13 02:36:23 +0000

    Set up input and database connection

--- database.py
+++ database.py
@@ -1,25 +1,25 @@
 import MySQLdb
 import yaml
 
-cfg = yaml.load(open('sql.yaml'))
+cfg = yaml.load(open('config.yaml'))
 
 def addUser(phoneNumber,channels) :
-	db = MySQLdb.connect(user=cfg['username'],passwd=cfg['password'],db=cfg['dbname'])
+	db = MySQLdb.connect(user=cfg['sql_username'],passwd=cfg['sql_password'],db=cfg['dbname'])
 	c = db.cursor()
 	c.executemany("""INSERT INTO users (user,channel) VALUES (%s, %s)""",[(phoneNumber,ch) for ch in channels])
 	db.commit()
 
 def numbersForChannel(channel) :
-	db = MySQLdb.connect(user=cfg['username'],passwd=cfg['password'],db=cfg['dbname'])
+	db = MySQLdb.connect(user=cfg['sql_username'],passwd=cfg['sql_password'],db=cfg['dbname'])
 	c = db.cursor()
 	c.execute("""SELECT user FROM users WHERE channel = %s""",channel)
 	return [res[0] for res in c.fetchall()]
 
 def getChannelsToCheck() :
-	db = MySQLdb.connect(user=cfg['username'],passwd=cfg['password'],db=cfg['dbname'])
+	db = MySQLdb.connect(user=cfg['sql_username'],passwd=cfg['sql_password'],db=cfg['dbname'])
 	c = db.cursor()
 	c.execute("""SELECT DISTINCT channel FROM users""")
 	return [res[0] for res in c.fetchall()]
 
-addUser("555-123-4567",["abcd","efgh","ijkl"])
-print [numbersForChannel(ch) for ch in getChannelsToCheck()]
+#addUser("555-123-4567",["abcd","efgh","ijkl"])
+#print [numbersForChannel(ch) for ch in getChannelsToCheck()]
